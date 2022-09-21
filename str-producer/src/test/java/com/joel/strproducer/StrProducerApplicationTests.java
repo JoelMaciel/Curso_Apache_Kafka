bc516036750b2b3c4b5ec077fbca8be047c8aeca
Joel Maciel <soldado25461@gmail.com>
2022-09-21 10:55:18 -0300

    str-producer started

---
+++ str-producer/src/test/java/com/joel/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
